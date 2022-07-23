3f4fff9a9368ff10a9ec3e7482d0015507676721
Guilherme <gdasilvarosario@gmail.com>
2022-07-23 13:00:24 -0300

    Testando novas configurações

--- telegrambot.py
+++ telegrambot.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
-driver.get('https://www.google.com/')
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://www.google.com")
+print(driver.page_source)
