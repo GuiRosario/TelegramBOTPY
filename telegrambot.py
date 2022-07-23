c8db86da564e8ab7272888f2c91c4a595306818b
Guilherme <gdasilvarosario@gmail.com>
2022-07-23 10:25:35 -0300

    Segundo Teste usando google como navegador agora mudando algumas configurações

--- telegrambot.py
+++ telegrambot.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 import os
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = webdriver.ChromeOptions()
+options = Options()
+options.add_argument("start-maximized")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get('https://www.google.com/')
 
