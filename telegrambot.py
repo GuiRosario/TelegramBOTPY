541dbc63220c996933a89063b10dc1d4f817c720
Guilherme <gdasilvarosario@gmail.com>
2022-07-22 21:14:24 -0300

    Atualizando

--- telegrambot.py
+++ telegrambot.py
@@ -4,13 +4,18 @@ from flask import Flask
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 from dotenv import load_dotenv
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import schedule
 
 app = Flask(__name__)
+link = 'https://ava.uft.edu.br/palmas/login/index.php'
+load_dotenv()
+CHAVE_API = os.getenv('CHAVE_API')
+USER_ID = os.getenv('USER_ID')
+username = os.getenv('username')
+senha = os.getenv('senha')
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -18,38 +23,32 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
+navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+navegador.get(url=link)
+bot = telebot.TeleBot(CHAVE_API)
+
+campo_username = navegador.find_element(By.ID,'username')
+campo_username.send_keys(username)
+campo_password = navegador.find_element(By.ID,'password')
+campo_password.send_keys(senha)
+botao_entrar = navegador.find_element(By.ID,'loginbtn')
+botao_entrar.click()
+
+
+def MandarMensagem():
+    materias = WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"h6.event-name.text-truncate.mb-0")))
+    bot.send_message(USER_ID, materias.text)
+
+schedule.every().day.at("21:15").do(MandarMensagem)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+bot.polling()
+
 @app.route('/')
-def TelegramBot():
-    link = 'https://ava.uft.edu.br/palmas/login/index.php'
-    CHAVE_API = os.getenv('CHAVE_API')
-    USER_ID = os.getenv('USER_ID')
-    username = os.getenv('username')
-    senha = os.getenv('senha')
+def olafilhodaputa():
+    return 'Olá filho da puta'
 
-    load_dotenv()
-    navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    navegador.get(url=link)
-    bot = telebot.TeleBot(CHAVE_API)
-
-    campo_username = navegador.find_element(By.ID,'username')
-    campo_username.send_keys(username)
-    campo_password = navegador.find_element(By.ID,'password')
-    campo_password.send_keys(senha)
-    botao_entrar = navegador.find_element(By.ID,'loginbtn')
-    botao_entrar.click()
-
-
-    def MandarMensagem():
-        materias = WebDriverWait(navegador, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"h6.event-name.text-truncate.mb-0")))
-        bot.send_message(USER_ID, materias.text)
-
-    schedule.every().day.at("19:48").do(MandarMensagem)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-    bot.polling()
-#if __name__ == "__main__":
-  #  port = int(os.environ.get("PORT",5000))
-   # app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=8080)
