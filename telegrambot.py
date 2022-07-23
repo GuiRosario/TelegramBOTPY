a34fcd71b987b002174adeecae2004ba160b0992
Guilherme <gdasilvarosario@gmail.com>
2022-07-23 10:30:12 -0300

    Arrumando A options

--- telegrambot.py
+++ telegrambot.py
@@ -6,10 +6,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("start-maximized")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get('https://www.google.com/')
