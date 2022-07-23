d03bd8abb2be1f8e0aafc373122e256a294ead36
Guilherme <gdasilvarosario@gmail.com>
2022-07-23 11:48:25 -0300

    Tentando usar a biblioteca selenium,adicionando driver manager

--- telegrambot.py
+++ telegrambot.py
@@ -1,10 +1,7 @@
-import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver = webdriver.Chrome(service=service)
-
-driver.get("https://google.com")
-
+driver.get('https://www.google.com/')
