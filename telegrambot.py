31a9773de1c2da35620e9c092eaaaf5752d345a4
Guilherme <gdasilvarosario@gmail.com>
2022-07-23 11:10:53 -0300

    Atualizando procfile

--- telegrambot.py
+++ telegrambot.py
@@ -1,16 +1,3 @@
-from selenium import webdriver
 import os
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
-driver.get('https://www.google.com/')
-
-print(driver.page_source)
